f4e552390eb9742a7232d29187c3cbe23186d689
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-31 13:59:56 +0300

    add supporting rocket files

---
+++ src/components/rockets/Rocket.js
@@ -0,0 +1,22 @@
+const Rocket = () => {
+  console.log('book added');
+  return (
+    <div>
+      <img src="" alt="rockets" />
+      <div>
+        <h2>Title</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+        <button type="button">Reserve Rocket</button>
+        <button type="button">Cancel Reservation</button>
+      </div>
+    </div>
+  );
+};
+export default Rocket;
