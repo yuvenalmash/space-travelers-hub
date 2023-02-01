97d22123011e54d9b36706f360d3e233710b2060
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-01 19:51:38 +0300

    List reserved rockets and joined missions

---
+++ src/components/myProfile/MyProfileLists.js
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+import { useSelector } from 'react-redux';
+import { selectAllRockets } from '../../redux/rockets/rocketsSlice';
+import { selectAllMissions } from '../../redux/missions/missionsSlice';
+
+const RocketItem = ({ rocketName }) => (
+  <li>{rocketName}</li>
+);
+
+const MissionItem = ({ missionName }) => (
+  <li>{missionName}</li>
+);
+
+const MyProfileLists = () => {
+  const rockets = useSelector(selectAllRockets);
+  const missions = useSelector(selectAllMissions);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
+  const joinedMissions = missions.filter((mission) => mission.joined === true);
+  return (
+    <>
+      <section>
+        <h2>My Missions</h2>
+        <ul>
+          {joinedMissions.map((mission) => (
+            <MissionItem
+              key={mission.id}
+              missionName={mission.name}
+            />
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h2>My Rockets</h2>
+        <ul>
+          {reservedRockets.map((rocket) => (
+            <RocketItem
+              key={rocket.id}
+              rocketName={rocket.name}
+            />
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default MyProfileLists;
