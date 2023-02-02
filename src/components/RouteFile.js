41ede212be49c242d1b05fa1ad5a6ee307996e30
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-02 17:38:12 +0300

    fix routes

--- src/components/RouteFile.js
+++ src/components/RouteFile.js
@@ -1,16 +1,16 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import Missions from './pages/Missions';
-import MyProfile from './pages/MyProfile';
-import Rockets from './pages/Rockets';
+import MissionsList from './missions/MissionList';
+import MyProfileLists from './myProfile/MyProfileLists';
+import RocketsList from './rockets/RocketsList';
 import View from './View';
 
 const RouteFile = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<View />}>
-        <Route index element={<Rockets />} />
-        <Route path="/missions" element={<Missions />} />
-        <Route path="/myProfile" element={<MyProfile />} />
+        <Route index element={<RocketsList />} />
+        <Route path="/missions" element={<MissionsList />} />
+        <Route path="/myProfile" element={<MyProfileLists />} />
       </Route>
     </Routes>
   </BrowserRouter>
