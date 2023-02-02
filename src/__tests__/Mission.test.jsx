59c4543ed6fa140d8e0e99a09814cbeea6838a2a
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-03 02:40:34 +0300

    Add more tests with react testing library

--- src/__tests__/Mission.test.jsx
+++ src/__tests__/Mission.test.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Mission from '../components/missions/Mission';
 import { Provider } from 'react-redux';
 import store from '../redux/ConfigureStore';
 
 const mission = {mission_id: '1', mission_name: 'Thaicom', description: 'description', joined: false}
+const mission2 = {mission_id: '1', mission_name: 'Thaicom', description: 'description', joined: true}
 
 describe('Display mission component', () => {
   test('it matches snapshot', () => {
@@ -23,5 +24,25 @@ describe('Display mission component', () => {
         <Mission mission = {mission} />
       </Provider>
     );
+  })
+})
+
+describe('Display Active banner text', () => {
+  test('Active banner to display \'Active member\'', () => {
+    render(
+      <Provider store={store}>
+        <Mission mission = {mission2} />
+      </Provider>
+    );
+    expect(screen.getByText('Active member')).toBeInTheDocument();
+  })
+
+  test('Active banner to display \'NOT A MEMBER\'', () => {
+    render(
+      <Provider store={store}>
+        <Mission mission = {mission} />
+      </Provider>
+    );
+    expect(screen.getByText('Not a member')).toBeInTheDocument();
   })
 })
