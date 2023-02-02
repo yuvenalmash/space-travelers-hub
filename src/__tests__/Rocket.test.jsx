e421bfeb83b67e0ae10400465cb330de6f3c3fa0
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-03 00:57:40 +0300

    fix failing display test

--- src/__tests__/Rocket.test.jsx
+++ src/__tests__/Rocket.test.jsx
@@ -5,7 +5,7 @@ import Rocket from '../components/rockets/Rocket';
 import { Provider } from 'react-redux';
 import store from '../redux/ConfigureStore';
 
-const rocket = {id: '1', name: 'falcon1', description: 'description', reserved: false}
+const rocket = {id: '1', name: 'falcon1', description: 'description', reserved: false, flickr_images: ['img1', 'img2']}
 describe('Display Rocket component', () => {
   test('it matches snapshot', () => {
     const tree = renderer.create(
